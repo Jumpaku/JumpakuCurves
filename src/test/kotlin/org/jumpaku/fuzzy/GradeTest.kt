e1cb4754604830f9c2e45763fc999459e209f357
Jumpaku <itzimeru@gmail.com>
2017-05-09 04:40:51 +0900

    implemented Grade and Membership

---
+++ src/test/kotlin/org/jumpaku/fuzzy/GradeTest.kt
@@ -0,0 +1,46 @@
+package org.jumpaku.fuzzy
+
+import org.assertj.core.api.Assertions.*
+import org.junit.Test
+
+/**
+ * Created by jumpaku on 2017/05/09.
+ */
+class GradeTest {
+    @Test
+    fun testToString() {
+        println("ToString")
+        fail("ToString not implemented.")
+    }
+
+    @Test
+    fun testCompareTo() {
+        println("CompareTo")
+        fail("CompareTo not implemented.")
+    }
+
+    @Test
+    fun testAnd() {
+        println("And")
+        fail("And not implemented.")
+    }
+
+    @Test
+    fun testOr() {
+        println("Or")
+        fail("Or not implemented.")
+    }
+
+    @Test
+    fun testNot() {
+        println("Not")
+        fail("Not not implemented.")
+    }
+
+    @Test
+    fun testGetValue() {
+        println("GetValue")
+        fail("GetValue not implemented.")
+    }
+
+}
