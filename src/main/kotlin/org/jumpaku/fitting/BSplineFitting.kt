69d8f3f9e8fc81dc438f84bfaca54ef6fded9ac3
Jumpaku <itzimeru@gmail.com>
2017-05-30 13:52:37 +0900

    implemented and tested SplineFitting and BezierFitting

---
+++ src/main/kotlin/org/jumpaku/fitting/BSplineFitting.kt
@@ -0,0 +1,43 @@
+package org.jumpaku.fitting
+
+import io.vavr.API.*
+import io.vavr.collection.Array
+import io.vavr.collection.Stream
+import org.apache.commons.math3.linear.MatrixUtils
+import org.apache.commons.math3.linear.QRDecomposition
+import org.jumpaku.affine.Point
+import org.jumpaku.curve.Knot
+import org.jumpaku.curve.bspline.BSpline
+import org.jumpaku.util.component1
+import org.jumpaku.util.component2
+import java.util.Comparator
+
+
+class BSplineFitting(val degree: Int, val knots: Array<Knot>) : Fitting<BSpline>{
+
+    fun basis(knotValues: Array<Double>, i: Int, t: Double): Double = BSpline.basis(t, degree, i, knotValues)
+
+    override fun fit(data: Array<TimeSeriesPoint>): BSpline {
+        val m = data.size() - 1
+        val us = knots.flatMap(Knot::toArray)
+        val sortedData = data.sorted(Comparator.comparing(TimeSeriesPoint::time))
+        val (d0, _) = sortedData[0]
+        val (dm, _) = sortedData[m]
+
+        val (ds, ts) = sortedData.subSequence(1, m)
+                .unzip { (v, t) -> Tuple(
+                        v.toVector() - d0.toVector() * basis(us, 0, t) - dm.toVector() * basis(us, (us.size() - degree - 1) - 1, t), t) }
+        val d = MatrixUtils.createRealMatrix(
+                ds.map { doubleArrayOf(it.x, it.y, it.z) }
+                        .toJavaArray(DoubleArray::class.java))
+        val b = MatrixUtils.createRealMatrix(
+                ts.map { t -> (1..((us.size() - degree - 1) - 2)).map { basis(us, it, t) } }
+                        .map(List<Double>::toDoubleArray)
+                        .toJavaArray(DoubleArray::class.java))
+        val p = QRDecomposition(b.transpose().multiply(b)).solver
+                .solve(b.transpose().multiply(d))
+                .run { Array(*this.data).map { Point.xyz(it[0], it[1], it[2]) } }
+
+        return BSpline(Stream.concat(Stream(d0), p, Stream(dm)), knots)
+    }
+}
--- src/main/kotlin/org/jumpaku/samples/Main.kt
+++ src/main/kotlin/org/jumpaku/samples/Main.kt
@@ -1,7 +1,13 @@
 package org.jumpaku.samples
 
+import io.vavr.API
+
 
 fun main(vararg args: String) {
 
-    println("Hello, world!")
+    val a = API.Array(1, 2, 3)
+    val b = a.prepend(4)
+
+    println("a = $a")
+    println("b = $b")
 }
