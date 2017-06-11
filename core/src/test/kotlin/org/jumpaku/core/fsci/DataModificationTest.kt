938c8f768838d75aea4f8f92edd8745ab2781e49
Jumpaku <itzimeru@gmail.com>
2017-06-12 03:33:24 +0900

    improved performance of FscGeneration

---
+++ core/src/test/kotlin/org/jumpaku/core/fsci/DataModificationTest.kt
@@ -0,0 +1,16 @@
+package org.jumpaku.core.fsci
+
+import org.assertj.core.api.Assertions.*
+import org.junit.Test
+
+/**
+ * Created by jumpaku on 2017/06/12.
+ */
+class DataModificationTest {
+    @Test
+    fun testModify() {
+        println("Modify")
+        fail("Modify not implemented.")
+    }
+
+}
