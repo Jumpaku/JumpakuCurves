e5762705bd29eac64b35c4c67f6b217dea355e31
Jumpaku <itzimeru@gmail.com>
2017-09-27 03:17:46 +0900

    tested PointSnapper

---
+++ fsc/src/test/kotlin/jumpaku/fsc/snap/PointSnapperTest.kt
@@ -0,0 +1,13 @@
+package jumpaku.fsc.snap
+
+import org.assertj.core.api.Assertions.*
+import org.junit.Test
+
+class PointSnapperTest {
+    @Test
+    fun testSnap() {
+        println("Snap")
+        fail("Snap not implemented.")
+    }
+
+}
