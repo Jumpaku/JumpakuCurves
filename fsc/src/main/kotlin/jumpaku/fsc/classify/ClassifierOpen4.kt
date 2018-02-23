7b278190f0405196103b881b3b61d01f7826624a
Jumpaku <itzimeru@gmail.com>
2018-02-24 01:48:05 +0900

    changed variable name muL --> pL

--- fsc/src/main/kotlin/jumpaku/fsc/classify/ClassifierOpen4.kt
+++ fsc/src/main/kotlin/jumpaku/fsc/classify/ClassifierOpen4.kt
@@ -11,15 +11,15 @@ class ClassifierOpen4(val nSamples: Int = 25, val nFmps: Int = 15) : Classifier 
 
     override fun classify(fsc: BSpline): ClassifyResult {
         val reparametrized = fsc.reparametrizeArcLength()
-        val muL = Linear.ofBeginEnd(reparametrized).isValidFor(reparametrized, nFmps)
-        val muC = Circular.ofBeginEnd(reparametrized).isValidFor(reparametrized, nFmps)
-        val muE = Elliptic.ofBeginEnd(reparametrized, nSamples).isValidFor(reparametrized, nFmps)
+        val pL = Linear.ofBeginEnd(reparametrized).isValidFor(reparametrized, nFmps)
+        val pC = Circular.ofBeginEnd(reparametrized).isValidFor(reparametrized, nFmps)
+        val pE = Elliptic.ofBeginEnd(reparametrized, nSamples).isValidFor(reparametrized, nFmps)
 
         return ClassifyResult(
-                CurveClass.LineSegment to (muL),
-                CurveClass.CircularArc to (!muL and muC),
-                CurveClass.EllipticArc to (!muL and !muC and muE),
-                CurveClass.OpenFreeCurve to (!muL and !muC and !muE)
+                CurveClass.LineSegment to (pL),
+                CurveClass.CircularArc to (!pL and pC),
+                CurveClass.EllipticArc to (!pL and !pC and pE),
+                CurveClass.OpenFreeCurve to (!pL and !pC and !pE)
         )
     }
 }
