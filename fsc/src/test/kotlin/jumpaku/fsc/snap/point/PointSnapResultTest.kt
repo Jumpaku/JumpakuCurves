d30d190e4276010ed2603f80309299f85b5d1639
Jumpaku <itzimeru@gmail.com>
2018-03-24 16:31:51 +0900

    tested PointSnapResult.toJson

--- fsc/src/test/kotlin/jumpaku/fsc/snap/point/PointSnapResultTest.kt
+++ fsc/src/test/kotlin/jumpaku/fsc/snap/point/PointSnapResultTest.kt
@@ -1,6 +1,10 @@
 package jumpaku.fsc.snap.point
 
+import jumpaku.core.affine.Point
+import jumpaku.core.affine.Vector
 import jumpaku.core.affine.pointAssertThat
+import jumpaku.core.json.parseJson
+import jumpaku.fsc.snap.Grid
 import jumpaku.fsc.snap.GridPoint
 import jumpaku.fsc.snap.gridAssertThat
 import jumpaku.fsc.snap.gridPointAssertThat
@@ -26,11 +30,25 @@ class PointSnapResultAssert(actual: PointSnapResult) : AbstractAssert<PointSnapR
 }
 
 class PointSnapResultTest {
+    
+    val baseGrid = Grid(
+            spacing = 1.0,
+            magnification = 4,
+            origin = Point.xyz(0.0, 0.0, 0.0),
+            axis = Vector.K,
+            radian = 0.0,
+            fuzziness = 0.25,
+            resolution = 0)
+
+    val snapper = PointSnapper(baseGrid, -1, 1)
 
     @Test
     fun testToString() {
         println("ToString")
-        fail("ToString not implemented.")
+        val r = 1/4.0
+        val e = snapper.snap(Point.xr( 7/32.0, r))
+        pointSnapResultAssertThat(e.toString().parseJson().flatMap { PointSnapResult.fromJson(it) }.get())
+                .isEqualToPointSnapResult(e)
     }
 
 }
