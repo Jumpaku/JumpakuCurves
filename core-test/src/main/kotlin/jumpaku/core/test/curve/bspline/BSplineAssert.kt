1ee78044f5926149dfd0329e705bf9c68dddc628
Jumpaku <itzimeru@gmail.com>
2018-04-27 22:56:48 +0900

    added fsc-test project

--- core-test/src/main/kotlin/jumpaku/core/test/curve/bspline/BSplineAssert.kt
+++ core-test/src/main/kotlin/jumpaku/core/test/curve/bspline/BSplineAssert.kt
@@ -1,2 +1,30 @@
-package jumpaku.core.test.curve.bspline 
+package jumpaku.core.test.curve.bspline
 
+import jumpaku.core.curve.bspline.BSpline
+import jumpaku.core.test.affine.pointAssertThat
+import jumpaku.core.test.curve.knotVectorAssertThat
+import jumpaku.core.util.component1
+import jumpaku.core.util.component2
+import org.assertj.core.api.AbstractAssert
+import org.assertj.core.api.Assertions
+
+fun bSplineAssertThat(actual: BSpline): BSplineAssert = BSplineAssert(actual)
+class BSplineAssert(actual: BSpline) : AbstractAssert<BSplineAssert, BSpline>(actual, BSplineAssert::class.java) {
+
+    fun isEqualToBSpline(expected: BSpline, eps: Double = 1.0e-10): BSplineAssert {
+        isNotNull
+
+        Assertions.assertThat(actual.controlPoints.size()).`as`("controlPoints size").isEqualTo(expected.controlPoints.size())
+
+        actual.controlPoints.zip(expected.controlPoints)
+                .forEachIndexed {
+                    i, (a, e) -> pointAssertThat(a).`as`("bSpline.controlPoints[%d]", i).isEqualToPoint(e, eps)
+                }
+
+        Assertions.assertThat(actual.knotVector.knots.size()).`as`("knotVector size").isEqualTo(expected.knotVector.knots.size())
+
+        knotVectorAssertThat(actual.knotVector).isEqualToKnotVector(expected.knotVector)
+
+        return this
+    }
+}
