2a4facc8e9af6334455ee1c856d6db81c7b34d77
Jumpaku <itzimeru@gmail.com>
2018-05-19 02:38:03 +0900

    refactored

--- fsc-test/src/test/kotlin/jumpaku/fsc/test/fragment/ChunkTest.kt
+++ fsc-test/src/test/kotlin/jumpaku/fsc/test/fragment/ChunkTest.kt
@@ -1,8 +1,9 @@
 package jumpaku.fsc.test.fragment
 
+import jumpaku.core.affine.Point
 import jumpaku.core.curve.Interval
+import jumpaku.core.curve.KnotVector
 import jumpaku.core.curve.bspline.BSpline
-import jumpaku.core.json.parseJson
 import jumpaku.core.test.shouldBeCloseTo
 import jumpaku.fsc.fragment.Chunk
 import jumpaku.fsc.fragment.TruthValueThreshold
@@ -12,35 +13,43 @@ import org.junit.Test
 
 class ChunkTest {
 
-    val urlString = "/jumpaku/fsc/test/fragment/"
-    fun resourceText(name: String): String = javaClass.getResource(urlString + name).readText()
-
-    private val fsc = resourceText("FragmenterTestFsc0.json").parseJson().flatMap { BSpline.fromJson(it) }.get()
-
-    private val eps = 1.0e-5
     private val threshold = TruthValueThreshold(0.4, 0.6)
 
+    val s0 = BSpline((0..3).map { Point.xr(it.toDouble(), it*3.0) }, KnotVector.uniform(Interval.ZERO_ONE, 1, 6))
+    val s1 = BSpline((0..3).map { Point.xr(it.toDouble(), (3.0 - it)*3) }, KnotVector.uniform(Interval.ZERO_ONE, 1, 6))
+    val s2 = BSpline((0..3).map { Point.xr(it.toDouble(), 9.0) }, KnotVector.uniform(Interval.ZERO_ONE, 1, 6))
+    val s3 = BSpline((0..3).map { Point.xr(it.toDouble(), 3.0) }, KnotVector.uniform(Interval.ZERO_ONE, 1, 6))
+
+    val c0 = chunk(s0, s0.domain, 4)
+    val c1 = chunk(s1, s1.domain, 4)
+    val c2 = chunk(s2, s2.domain, 4)
+    val c3 = chunk(s3, s3.domain, 4)
+
     @Test
-    fun testState() {
-        println("State")
-        val c1 = chunk(fsc, Interval(106535.12613733625, 106535.20095651352), 4)
-        c1.state(threshold).shouldBe(Chunk.State.MOVE)
-        val c2 = chunk(fsc, Interval(106535.45035377113, 106535.52517294842), 4)
-        c2.state(threshold).shouldBe(Chunk.State.STAY)
-        val c3 = chunk(fsc, Interval(106535.55011267416, 106535.62493185145), 4)
-        c3.state(threshold).shouldBe(Chunk.State.UNKNOWN)
+    fun testChunkCreate() {
+        println("ChunkState")
+        c0.state(threshold).shouldBe(Chunk.State.UNKNOWN)
+        c1.state(threshold).shouldBe(Chunk.State.STAY)
+        c2.state(threshold).shouldBe(Chunk.State.UNKNOWN)
+        c3.state(threshold).shouldBe(Chunk.State.MOVE)
     }
 
     @Test
     fun testNecessity() {
-        val c = chunk(fsc, Interval(106535.55011267416, 106535.62493185145), 4)
-        c.necessity.value.shouldBeCloseTo(0.042337862693934136, eps)
+        println("Necessity")
+        c0.necessity.value.shouldBeCloseTo(0.0)
+        c1.necessity.value.shouldBeCloseTo(2/3.0)
+        c2.necessity.value.shouldBeCloseTo(1/3.0)
+        c3.necessity.value.shouldBeCloseTo(0.0)
     }
 
     @Test
     fun testPossibility() {
-        val c = chunk(fsc, Interval(106535.55011267416, 106535.62493185145), 4)
-        c.possibility.value.shouldBeCloseTo(0.8946264226651022, eps)
+        println("Possibility")
+        c0.possibility.value.shouldBeCloseTo(2/3.0)
+        c1.possibility.value.shouldBeCloseTo(2/3.0)
+        c2.possibility.value.shouldBeCloseTo(2.5/3)
+        c3.possibility.value.shouldBeCloseTo(0.5)
     }
 
 }
