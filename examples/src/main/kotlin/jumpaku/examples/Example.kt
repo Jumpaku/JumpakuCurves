fe36ddff787b22e77fa78308bd629385530aa3d2
Jumpaku <itzimeru@gmail.com>
2018-10-05 03:37:09 +0900

    completed Colors.

--- examples/src/main/kotlin/jumpaku/examples/Example.kt
+++ examples/src/main/kotlin/jumpaku/examples/Example.kt
@@ -1,19 +1,17 @@
 package jumpaku.examples
 
-import io.vavr.collection.Array
 import javafx.application.Application
 import javafx.scene.layout.Pane
-import javafx.scene.paint.Color
-import javafx.scene.shape.Circle
-import jumpaku.core.curve.arclength.Reparametrizer
-import jumpaku.core.curve.arclength.repeatBisect
-import jumpaku.core.curve.bspline.BSpline
-import jumpaku.core.curve.polyline.Polyline
-import jumpaku.core.geom.Point
-import jumpaku.fsc.generate.FscGenerator
-import jumpaku.fxcomponents.nodes.*
-import org.apache.commons.math3.util.FastMath
-import tornadofx.*
+import jumpaku.fxcomponents.colors.CudPalette
+import jumpaku.fxcomponents.colors.fx
+import jumpaku.fxcomponents.nodes.curve
+import jumpaku.fxcomponents.nodes.fscUpdateControl
+import jumpaku.fxcomponents.nodes.fuzzyCurve
+import jumpaku.fxcomponents.nodes.onFscUpdated
+import tornadofx.App
+import tornadofx.View
+import tornadofx.group
+import tornadofx.pane
 
 
 fun main(vararg args: String) = Application.launch(AppExample::class.java, *args)
@@ -30,8 +28,8 @@ class ViewExample : View() {
                 with(group) {
                     children.clear()
                     val s = e.fsc
-                    curve(s) { stroke = CudPalette.RED }
-                    fuzzyCurve(s) { stroke = CudPalette.BLUE }
+                    curve(s) { stroke = CudPalette.RED.fx() }
+                    fuzzyCurve(s) { stroke = CudPalette.BLUE.fx() }
                 }
             }
         }
