57c2f47637ea915b77daa00a95b48b8a7e8b3180
Jumpaku <itzimeru@gmail.com>
2019-02-26 03:26:54 +0900

    removed from core.test.curve.polyline

--- core-test/src/main/kotlin/jumpaku/curves/core/test/curve/polyline/PolylineMatcher.kt
+++ core-test/src/main/kotlin/jumpaku/curves/core/test/curve/polyline/PolylineMatcher.kt
@@ -2,12 +2,18 @@ package jumpaku.curves.core.test.curve.polyline
 
 import jumpaku.curves.core.curve.polyline.Polyline
 import jumpaku.curves.core.test.geom.isCloseTo
+import jumpaku.curves.core.test.matcher
 import org.amshove.kluent.should
+import org.hamcrest.TypeSafeMatcher
 
 fun isCloseTo(actual: Polyline, expected: Polyline, error: Double = 1.0e-9): Boolean =
         (actual.points.size == expected.points.size) &&
                 actual.points.zip(expected.points) { a, e -> isCloseTo(a, e, error) }.all { it }
 
+fun closeTo(expected: Polyline, precision: Double = 1.0e-9): TypeSafeMatcher<Polyline> =
+        matcher("close to <$expected> with precision $precision") { actual ->
+            isCloseTo(actual, expected, precision)
+        }
 
 fun Polyline.shouldEqualToPolyline(expected: Polyline, error: Double = 1.0e-9) = this.should("$this should be $expected") {
     isCloseTo(this, expected, error)
