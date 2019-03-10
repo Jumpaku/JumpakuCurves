0b9b872a7ae17d2c1a92323593fc137738869ed3
Jumpaku <itzimeru@gmail.com>
2019-03-10 20:07:24 +0900

    implemented and tested PointSnapper.toJson()

---
+++ jumpaku-curves-fsc-test/src/main/kotlin/jumpaku/curves/fsc/test/snap/point/PointSnapperMatcher.kt
@@ -0,0 +1,17 @@
+package jumpaku.curves.fsc.test.snap.point
+
+import jumpaku.commons.test.matcher
+import jumpaku.curves.fsc.snap.point.MFGS
+import jumpaku.curves.fsc.snap.point.PointSnapper
+import org.hamcrest.TypeSafeMatcher
+
+fun equals(actual: PointSnapper, expected: PointSnapper): Boolean = when(actual) {
+    is MFGS -> expected is MFGS &&
+            actual.minResolution == expected.minResolution &&
+            actual.maxResolution == expected.maxResolution
+}
+
+fun equalTo(expected: PointSnapper): TypeSafeMatcher<PointSnapper> =
+        matcher("close to <$expected>") { actual ->
+            equals(actual, expected)
+        }
