9563356e86a66587a69352c74ceaec5229f13407
Jumpaku <itzimeru@gmail.com>
2019-03-10 13:01:14 +0900

    tested Blender

--- jumpaku-curves-demo/src/main/kotlin/jumpaku/curves/demo/blend/BlendDemo.kt
+++ jumpaku-curves-demo/src/main/kotlin/jumpaku/curves/demo/blend/BlendDemo.kt
@@ -5,16 +5,19 @@ import javafx.scene.Scene
 import javafx.scene.input.KeyCode
 import javafx.scene.input.KeyEvent
 import javafx.stage.Stage
-import jumpaku.commons.control.*
+import jumpaku.commons.control.Option
+import jumpaku.commons.control.none
+import jumpaku.commons.control.some
 import jumpaku.curves.core.curve.bspline.BSpline
 import jumpaku.curves.core.fuzzy.Grade
-import jumpaku.curves.core.geom.Point
 import jumpaku.curves.fsc.blend.Blender
-import jumpaku.curves.fsc.blend.OverlapMatrix
 import jumpaku.curves.fsc.generate.DataPreparer
 import jumpaku.curves.fsc.generate.Generator
 import jumpaku.curves.fsc.generate.LinearFuzzifier
-import jumpaku.curves.graphics.*
+import jumpaku.curves.graphics.DrawStyle
+import jumpaku.curves.graphics.clearRect
+import jumpaku.curves.graphics.drawCubicBSpline
+import jumpaku.curves.graphics.drawPoints
 import jumpaku.curves.graphics.fx.DrawingControl
 import jumpaku.curves.graphics.fx.DrawingEvent
 import java.awt.Color
@@ -57,32 +60,9 @@ class BlendDemo : Application() {
                 updateGraphics2D {
                     clearRect(0.0, 0.0, width, height)
                     existingFscOpt.forEach { drawCubicBSpline(it) }
-                    existingFscOpt.ifAbsent { drawPoints(it.drawingStroke.inputData.map { it.point.copy(r = 3.0) }) }
-                    //existingFscOpt.forEach { drawPoints(it.evaluateAll(0.01)) }
+                    existingFscOpt.forEach { drawPoints(it.evaluateAll(0.01)) }
 
                     val overlappingFsc = BlendDemoSettings.generator.generate(it.drawingStroke.inputData)
-
-                    existingFscOpt.forEach { existing ->
-                        val samplingSpan = BlendDemoSettings.blender.samplingSpan
-                        val existSamples = existing.sample(samplingSpan)
-                        val overlapSamples = overlappingFsc.sample(samplingSpan)
-                        val osm = OverlapMatrix.create(existSamples.map { it.point }, overlapSamples.map { it.point })
-                        val path = BlendDemoSettings.blender.findPath(osm)
-                        fun point(i: Int, j: Int): Point = Point.xyr(i*5.0, j*5.0, 1.0)
-                        for (y in 0..osm.rowLastIndex) {
-                            for (x in 0..osm.columnLastIndex) {
-                                drawPoint(point(y, x), DrawStyle(
-                                        color = if (osm[y, x] >= BlendDemoSettings.blender.possibilityThreshold) Color.BLUE
-                                        else Color.LIGHT_GRAY))
-                            }
-                        }
-                        path.forEach {
-                            it.map { (y, x) -> point(y, x) }.forEach { drawPoint(it, DrawStyle(Color.RED)) }
-                            drawPoints(((0 until it.first().first) + ((it.last().first + 1) until existSamples.size)).map { i -> existSamples[i].point.copy(r = 3.0) }, DrawStyle(Color.GREEN))
-                            drawPoints(((0 until it.first().second) + ((it.last().second + 1) until overlapSamples.size)).map { j -> overlapSamples[j].point.copy(r = 3.0) }, DrawStyle(Color.GREEN))
-                            drawPoints(it.map { (i, j) -> existSamples[i].lerp(BlendDemoSettings.blender.blendingRate, overlapSamples[j]).point.copy(r = 3.0) }, DrawStyle(Color.BLUE))
-                        }
-                    }
 
                     existingFscOpt.ifPresent { existingFsc ->
                         BlendDemoSettings.blender.blend(existingFsc, overlappingFsc).forEach {
@@ -92,8 +72,8 @@ class BlendDemo : Application() {
                         existingFscOpt = some(overlappingFsc)
                     }
 
-                    //drawCubicBSpline(overlappingFsc, DrawStyle(Color.CYAN))
-                    //drawPoints(overlappingFsc.evaluateAll(0.01), DrawStyle(Color.CYAN))
+                    drawCubicBSpline(overlappingFsc, DrawStyle(Color.CYAN))
+                    drawPoints(overlappingFsc.evaluateAll(0.01), DrawStyle(Color.CYAN))
                     existingFscOpt.forEach { drawCubicBSpline(it, DrawStyle(Color.MAGENTA)) }
                     existingFscOpt.forEach { drawPoints(it.evaluateAll(0.01), DrawStyle(Color.MAGENTA)) }
                 }
@@ -101,7 +81,7 @@ class BlendDemo : Application() {
         }
         val scene = Scene(curveControl).apply {
             addEventHandler(KeyEvent.KEY_PRESSED) {
-                if (it.code == KeyCode.C) {
+                if (it.code == KeyCode.C) {//clear
                     curveControl.updateGraphics2D { clearRect(0.0, 0.0, width, height) }
                     existingFscOpt = none()
                 }
