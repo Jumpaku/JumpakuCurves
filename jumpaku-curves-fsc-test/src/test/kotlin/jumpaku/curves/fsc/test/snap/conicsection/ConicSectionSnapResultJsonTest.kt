1040c8fcafca0b5fca25ce41816f5a17cfe407b0
Jumpaku <Jumpaku@users.noreply.github.com>
2021-08-12 03:14:24 +0900

    Update ConicSectionSnapResultJsonTest.kt

--- jumpaku-curves-fsc-test/src/test/kotlin/jumpaku/curves/fsc/test/snap/conicsection/ConicSectionSnapResultJsonTest.kt
+++ jumpaku-curves-fsc-test/src/test/kotlin/jumpaku/curves/fsc/test/snap/conicsection/ConicSectionSnapResultJsonTest.kt
@@ -12,9 +12,9 @@ class ConicSectionSnapResultJsonTest {
     val urlString = "/jumpaku/curves/fsc/test/snap/conicsection/"
     init {
         val classPaths = System.getProperty("java.class.path").split(":")
-        if(!classPaths.any { it.startsWith("/home/runner/work/JumpakuCurves/JumpakuCurves/") })
+        if(!classPaths.any { it.startsWith("/project") })
             error("Error!:")
-        if(!classPaths.any { it.endsWith("JumpakuCurves/jumpaku-curves-fsc-test/build/resources/test") })
+        if(!classPaths.any { it.endsWith("/project/jumpaku-curves-fsc-test/build/resources/test") })
             error("Error!:")
         val t = "${this::class.java.getResource(urlString)}"
         if (!t.endsWith("/jumpaku/curves/fsc/test/snap/conicsection/"))
