29f1e51e06ecc38749b14f0203a48cf7587bccf6
Jumpaku <Jumpaku@users.noreply.github.com>
2021-08-12 15:59:36 +0900

    refactor: make Calibrate.matrix not lazy

--- jumpaku-curves-core/src/main/kotlin/jumpaku/curves/core/transform/Calibrate.kt
+++ jumpaku-curves-core/src/main/kotlin/jumpaku/curves/core/transform/Calibrate.kt
@@ -6,10 +6,11 @@ import jumpaku.curves.core.geom.times
 import org.apache.commons.math3.linear.*
 
 class Calibrate(
-        val pair0: Pair<Point, Point>,
-        val pair1: Pair<Point, Point>,
-        val pair2: Pair<Point, Point>,
-        val pair3: Pair<Point, Point>) : Transform {
+    val pair0: Pair<Point, Point>,
+    val pair1: Pair<Point, Point>,
+    val pair2: Pair<Point, Point>,
+    val pair3: Pair<Point, Point>
+) : Transform {
 
     constructor(pair0: Pair<Point, Point>, pair1: Pair<Point, Point>, pair2: Pair<Point, Point>)
             : this(makePairs(pair0, pair1, pair2))
@@ -22,14 +23,16 @@ class Calibrate(
 
     private constructor(pairs4: List<Pair<Point, Point>>) : this(pairs4[0], pairs4[1], pairs4[2], pairs4[3])
 
-    override val matrix: RealMatrix by lazy {
+    override val matrix: RealMatrix
+
+    init {
         val from = arrayOf(pair0, pair1, pair2, pair3)
-                .map { (f, _) -> (f.toDoubleArray() + 1.0) }
-                .run { (MatrixUtils.createRealMatrix(toTypedArray())) }
+            .map { (f, _) -> (f.toDoubleArray() + 1.0) }
+            .run { (MatrixUtils.createRealMatrix(toTypedArray())) }
         val to = arrayOf(pair0, pair1, pair2, pair3)
-                .map { (_, t) -> (t.toDoubleArray() + 1.0) }
-                .run { (MatrixUtils.createRealMatrix(toTypedArray())) }
-        QRDecomposition(from).solver.solve(to).transpose()
+            .map { (_, t) -> (t.toDoubleArray() + 1.0) }
+            .run { (MatrixUtils.createRealMatrix(toTypedArray())) }
+        matrix = QRDecomposition(from).solver.solve(to).transpose()
     }
 
     companion object {
@@ -41,14 +44,22 @@ class Calibrate(
             val (fromB, toB) = pair2
             val u = fromA - fromO
             val v = fromB - fromO
-            val m = Array2DRowRealMatrix(arrayOf(
+            val m = Array2DRowRealMatrix(
+                arrayOf(
                     doubleArrayOf(u.square(), u.dot(v)),
-                    doubleArrayOf(u.dot(v), v.square())))
+                    doubleArrayOf(u.dot(v), v.square())
+                )
+            )
             val solver = QRDecomposition(m).solver
             return listOf(Vector.Zero, Vector.I, Vector.J, Vector.K).map {
-                val (s, t) = solver.solve(ArrayRealVector(doubleArrayOf(
-                        it.dot(u),
-                        it.dot(v)))).toArray()
+                val (s, t) = solver.solve(
+                    ArrayRealVector(
+                        doubleArrayOf(
+                            it.dot(u),
+                            it.dot(v)
+                        )
+                    )
+                ).toArray()
                 (fromO + it) to Point((1 - s - t) * toO.toVector() + s * toA.toVector() + t * toB.toVector())
             }
         }
@@ -58,18 +69,19 @@ class Calibrate(
             val (fromP, toP) = pair1
             val v = fromP - fromO
             return listOf(Vector.Zero, Vector.I, Vector.J, Vector.K)
-                    .map { fromO + it to toO.lerp(v.dot(it) / v.square(), toP) }
+                .map { fromO + it to toO.lerp(v.dot(it) / v.square(), toP) }
         }
 
         private fun makePairs(pair0: Pair<Point, Point>): List<Pair<Point, Point>> {
             val (from, to) = pair0
             return listOf(Vector.Zero, Vector.I, Vector.J, Vector.K)
-                    .map { from + it to to }
+                .map { from + it to to }
         }
 
 
         fun similarityWithNormal(
-                pair0: Pair<Point, Point>, pair1: Pair<Point, Point>, normal: Pair<Vector, Vector>): Calibrate {
+            pair0: Pair<Point, Point>, pair1: Pair<Point, Point>, normal: Pair<Vector, Vector>
+        ): Calibrate {
             val (fromO, toO) = pair0
             val (fromP, toP) = pair1
             val (fromN, toN) = normal.let { (f, t) -> f.normalize().orThrow() to t.normalize().orThrow() }
