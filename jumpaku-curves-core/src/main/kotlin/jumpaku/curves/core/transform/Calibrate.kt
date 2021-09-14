367cbfbca842cbac73751149b03bf83b7c2dd51a
Jumpaku <Jumpaku@users.noreply.github.com>
2021-09-14 19:15:06 +0900

    feat(core): add Similarity

--- jumpaku-curves-core/src/main/kotlin/jumpaku/curves/core/transform/Calibrate.kt
+++ jumpaku-curves-core/src/main/kotlin/jumpaku/curves/core/transform/Calibrate.kt
@@ -22,6 +22,7 @@ class Calibrate private constructor(override val matrix: RealMatrix) : Transform
     constructor(pair0: Pair<Point, Point>) : this(makeMatrix(pair0))
 
     companion object {
+
         fun makeMatrix(
             pair0: Pair<Point, Point>,
             pair1: Pair<Point, Point>,
